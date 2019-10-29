b5e37fb752f34acd4fbce0a7fa21d77d2b60513d
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2019-10-29 12:47:48 -0700

    merged user and bank_account

--- user_class.py
+++ user_class.py
@@ -1,29 +1,32 @@
+class BankAccount:
+  def __init__(self, int_rate, balance):
+    self.int_rate = int_rate
+    self.balance = balance
+
+  def deposit(self, amount):
+    self.balance += amount
+    return self
+
+  def withdraw(self, amount):
+    self.balance -= amount
+    return self
+
+  def display_account_info(self):
+    print("Account balance: $" + str(self.balance) +
+          "\n" + "Interest rate:", self.int_rate)
+    return self
+
+  def yield_interest(self):
+    self.balance += self.balance * self.int_rate / 100
+    return self
+
 class User:
-  def __init__(self, name, email):
+  def __init__(self, name, email, int_rate, balance):
     self.name = name
     self.email = email
-    self.account_balance = 0
+    self.account = BankAccount(int_rate, balance)
 
-  def make_deposit(self, amount):
-    self.account_balance += amount
-    return self
+pidr = User('Pidr', 'pidr@com', 3, 1000)
+gnida = User('Gnida', 'gnida@com', 2, 2000)
 
-  def make_withdrawal(self, amount):
-    self.account_balance -= amount
-    return self
-
-  def display_user_balance(self):
-    print(self.name, ": Balance $" + str(self.account_balance))
-    return self
-    
-  def transfer_money(self, other_user, amount):
-    self.make_withdrawal(amount)
-    other_user.make_deposit(amount)
-    return self
-
-pidr = User('Pidr', 'pidr@com')
-gnida = User('Gnida', 'gnida@com')
-
-pidr.make_deposit(100).display_user_balance().transfer_money(gnida, 50)
-gnida.display_user_balance()
-pidr.display_user_balance()
+pidr.account.deposit(500).display_account_info()
