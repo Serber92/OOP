d1671945df479959c077ed26b4b61bbfd2dc3fb2
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2019-10-28 18:24:04 -0700

    added chaining to user_class.py

--- user_class.py
+++ user_class.py
@@ -6,22 +6,24 @@ class User:
 
   def make_deposit(self, amount):
     self.account_balance += amount
+    return self
 
   def make_withdrawal(self, amount):
     self.account_balance -= amount
+    return self
 
   def display_user_balance(self):
     print(self.name, ": Balance $" + str(self.account_balance))
+    return self
     
   def transfer_money(self, other_user, amount):
     self.make_withdrawal(amount)
     other_user.make_deposit(amount)
+    return self
 
 pidr = User('Pidr', 'pidr@com')
 gnida = User('Gnida', 'gnida@com')
 
-pidr.make_deposit(100)
-pidr.display_user_balance()
-pidr.transfer_money(gnida, 50)
+pidr.make_deposit(100).display_user_balance().transfer_money(gnida, 50)
 gnida.display_user_balance()
 pidr.display_user_balance()
